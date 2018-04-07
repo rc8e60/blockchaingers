35f698b3e4d4b01f3ad7e086636d5fe3fce6415f
Daan <daan@foryard.nl>
2018-04-07 16:15:16 +0200

    Corporation create view

---
+++ resources/views/corporation/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create your company</div>
+
+                <div class="panel-body">
+                    {!! Form::open(['action' => 'CorporationController@store']) !!}
+                        <div class="col-md-6">
+                            <div class="form-group">
+                            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Your company name here']) !!}
+                            </div>
+                        </div>
+                        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
