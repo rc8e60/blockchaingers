19b08fd8368c599adad70ad366ee7c242c997d49
rc8e60 <rc8e60@foryard.nl>
2018-04-07 17:08:53 +0200

    Authentication redirects changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/corporation/create';
 
     /**
      * Create a new controller instance.
